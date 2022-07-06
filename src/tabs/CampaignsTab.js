be847f402cfb18c6a3e929b8c957f7ebb767e967
JosepMP <josepmarchesparra@gmail.com>
2022-07-06 21:22:27 +0200

    Intervalo fechas arreglado

--- src/tabs/CampaignsTab.js
+++ src/tabs/CampaignsTab.js
@@ -11,9 +11,13 @@ export default function CampaingsTab({...props}) {
 
     const [state, setState] = useState();
     const [intervalos, setIntervalos] = useState([]);
-    const [textoIntervalo, setTextoIntervalo] = useState();
     const [currentAddress, setCurrentAddress] = useState("");
     const [items, setItems] = useState([]);
+    const [fechaPorDefecto, setFechaPorDefecto] = useState("");
+
+    const [textoIntervalo, setTextoIntervalo] = useState();
+    const [evento, setEvento] = useState();
+    
     
 
     function getWeeksIntervals(){
@@ -38,6 +42,7 @@ export default function CampaingsTab({...props}) {
             fechafinal = moment(fechainicial).add(6, 'days');    
             intervalos.push({"fechainicial" : fechainicial.format('YYYY-MM-DD') , "fechafinal" :fechafinal.format('YYYY-MM-DD')});
         }
+        setFechaPorDefecto(cutDate(intervalos[0].fechainicial )+ ' ' + cutDate(intervalos[0].fechafinal));
 
         return intervalos
         
@@ -53,17 +58,20 @@ export default function CampaingsTab({...props}) {
            return fechaEscrita;
     }
     
-    async function getData(online){
+    async function getData(){
         var items_list = [];
 
         items_list = await getEventsListFromBlockchain();
 
         setItems(items_list)
         
+        
     }
 
-    useEffect(() => {
+        useEffect(() => {
         setIntervalos(getWeeksIntervals());
+        getData();
+        
     }, []);
 
     // 3 desplehables - numero de dias - 
@@ -74,22 +82,22 @@ export default function CampaingsTab({...props}) {
             <NavBarWithSearchBar searchBar={false} applySearchFilter={(value) => null/*applySearchFilter(value)*/}/>
             <Flex direction={"column"} mt={Dimensions.navBar.TOP_MENU_HEIGHT} p={4}>
 
-                <Flex flex={1} direction={'row'} borderRadius={'10px'} p={4} borderWidth={'1px'} bg={'white'} mb={"16px"}>
-<<<<<<< Updated upstream
-                    <Text fontSize="xl" fontWeight="700">18 - 24 de Julio de 2022</Text>
-=======
-                    <Text fontSize="xl" fontWeight="700">{textoIntervalo}</Text>
-                    <Select placeholder='Selecciona semana' w={300} onChange = {(e) => setTextoIntervalo(e.target.value)}>
+                <Flex flex={1} alignItems={{base:'center',lg:'none'}} direction={{base:'column',lg:'row'}} borderRadius={'10px'} p={4} borderWidth={'1px'} bg={'white'} mb={"16px"}>
+                    <Text fontSize="xl" fontWeight="700">{textoIntervalo == null ? fechaPorDefecto : textoIntervalo}</Text>
+                    <Select ml={{base:'none', lg:'auto'}}  mt={{base:'2', lg:'none'}} w={250} onChange = {(e) => setTextoIntervalo(e.target.value)}>
                         {intervalos.length > 0 ? 
                                             intervalos.map((intervalo) => ( 
-                                                <option value={cutDate(intervalo.fechainicial) + '-' + cutDate(intervalo.fechafinal) }>{cutDate(intervalo.fechainicial)} - {cutDate(intervalo.fechafinal)}</option>
+                                                <option value={cutDate(intervalo.fechainicial) + '-' + cutDate(intervalo.fechafinal)}>{cutDate(intervalo.fechainicial)} - {cutDate(intervalo.fechafinal)}</option>
                                             ))
                                         : null}
                     </Select>
-                    <Select placeholder='Selecciona evento' w={300} onChange = {(e) => setTextoIntervalo(e.target.value)}>
-                        
+                    <Select w={250} placeholder='Selecciona evento' mt={{base:'2', lg:'none'}} ml={{base:'none', lg:'2'}} onChange = {(e) => setEvento(e.target.value)}>
+                        {items.length > 0 ? 
+                                                items.map((items) => ( 
+                                                    <option value={items._id}>{items.title}</option>
+                                                ))
+                                            : null}
                     </Select>
->>>>>>> Stashed changes
                 </Flex>
 
                 {/*<Flex flex={1} direction={'column'} borderRadius={'10px'} p={4} borderWidth={'1px'} bg={'white'} mb={"16px"}>
