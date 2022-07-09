17993097c236e8614f93ba82c99e3e7e5fadd048
albertgranados <albertgrro@gmail.com>
2022-07-09 14:20:21 +0200

    Updateado botón

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,9 @@ import { Text, Center, Flex } from '@chakra-ui/react';
 
 export default function Button({...props}) {
     return (
-        <Center as={"button"} h={"50px"} w={"full"} bg={props.bg ?? "black"} borderRadius={"10px"} mt={props.mt ?? "0px"} _hover={{bg: props.bghover ?? '#1a1c1c'}} style={{WebkitTapHighlightColor: "transparent"}} onClick={() => props.onClick()} transition="all .6s ease">
+        <Center as={props.disabled == true ? null : "button"} disabled={props.disabled} h={"50px"} w={"full"} bg={props.disabled == true ? 'gray.200' : props.bg ?? "black"} borderRadius={"10px"} mt={props.mt ?? "0px"} _hover={{bg: props.disabled == true ? null : props.bghover ?? '#1a1c1c'}} style={{WebkitTapHighlightColor: "transparent"}} onClick={() => props.onClick()} transition="all .6s ease">
             {props.icon ?? null }
-            <Text color={props.color ?? "white"} fontWeight={500} pl={props.icon ? '6px' : '18px'} pr={'18px'}>{props.text}</Text>
+            <Text color={props.disabled == true ? 'black' : props.color ?? "white"} fontWeight={500} pl={props.icon ? '6px' : '18px'} pr={'18px'}>Agotado</Text>
         </Center>
     )
 };
